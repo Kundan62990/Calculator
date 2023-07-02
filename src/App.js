b7fe211dc8228f1fb8f37b146e52377e714024db
kundan <kk7704437@gmail.com>
2023-07-02 14:15:45 +0530

    calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import './App.css'
 
-function App() {
+const App=()=> {
+ let [inputText ,setInputText]=useState("");
+
+ const buttonClick=(event)=>{
+   let clickedButton=event.target;
+   let value= clickedButton.innerHTML;
+   setInputText(inputText + value)
+ }
+  
+ const answer =()=>{
+  try {
+    let ans = eval(inputText)
+    setInputText(ans)
+  } catch (err) {
+        setInputText("Invalid syntax")
+  }
+ }
+
+ const clear=()=>{
+  setInputText("");
+ }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div>
+          <input className="calculator_input" type="text" value={inputText} readOnly />
+          <div className="button_container">
+            <button onClick={clear}>C</button>
+            <button onClick={buttonClick}>/</button>
+            <button onClick={buttonClick}>*</button>
+            <button onClick={buttonClick}>-</button>
+            <button onClick={buttonClick}>7</button>
+            <button onClick={buttonClick}>8</button>
+            <button onClick={buttonClick}>9</button>
+            <button onClick={buttonClick}>+</button>
+            <button onClick={buttonClick}>4</button>
+            <button onClick={buttonClick}>5</button>
+            <button onClick={buttonClick}>6</button>
+            <button onClick={buttonClick}>.</button>
+            <button onClick={buttonClick}>1</button>
+            <button onClick={buttonClick}>2</button>
+            <button onClick={buttonClick}>3</button>
+            <button onClick={answer}>=</button>
+            <button onClick={buttonClick}>0</button>
+          </div>
+        </div>
   );
 }
 
